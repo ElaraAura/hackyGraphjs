7f3af504c5dac8e08c02cddbd7c6e63ca02bdc76
Joel Berta <Joel Berta>
2017-11-10 22:41:57 +1100

    fix bug

--- graphGenerator.js
+++ graphGenerator.js
@@ -4,14 +4,17 @@ var amountOfNodes
 var graph
 
 function determineConnections(current){
+    var noNodeAdded = false
      for(var i = 1; i <= amountOfNodes; i++){
         if(i != current){
             var chance = Math.random()
             if(chance < connectionChance){
+                noNodeAdded = true
                 graph += `${i},`
             }
         }
     }
+    if(!noNodeAdded) graph +=","
 }
 
 function constructGraph(){
@@ -28,7 +31,7 @@ function main(){
     connectionChance = 0.1
     var numberOfGraphs = 20
     for(var i = 0; i < numberOfGraphs; i++){
-        graph = ""
+        graph = ""+numberOfGraphs+"\n"
         constructGraph()
         fs.writeFile(`customGraph-${i}.txt`,graph,(err) => {
             if (err) console.log("oops!", err)
